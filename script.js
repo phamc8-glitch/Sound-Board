7b22bfa18c4568f1c58c0eeaebcd27596ea63f96
Cindy <phamc8@vcu.edu>
2026-01-29 12:40:41 -0500

    script.js modifcations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create an array of sounds
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ['aww','applause', 'tada', 'right', 'victory', 'eww', 'boo', 'gasp', 'wrong', 'failure', 'laugh', 'wow'];
 
 // For each sound, create a button, add a class 'btn', update the text, append to DOM
 sounds.forEach((sound) => {
@@ -11,7 +11,24 @@ sounds.forEach((sound) => {
     
     // Set the button label text to be the same as the string value in the sound array
     btn.innerText = sound;
+
+    // Add an event listener to this button
+    btn.addEventListener('click', () => {
+        // Stop all sounds first
+        stopSounds();
+        document.getElementById(sound).play();
+    });
     
     // Add the button to the DOM
     document.getElementById('buttons').appendChild(btn);
 });
+
+// Helper functions
+function stopSounds() {
+    sounds. forEach((sound) => {
+        const snd = document.getElementById(sound);
+
+        snd.pause();
+        snd.currentTime = 0;
+    })
+}
